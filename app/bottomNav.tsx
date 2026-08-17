615610ce9ca30f255e5fe6b01cd92537c3776eff
Niccolò Sfregola <Niccolo.Sfregola@wmi.badw.de>
2026-08-17 21:46:23 +0200

    fancy bars

--- app/bottomNav.tsx
+++ app/bottomNav.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function BottomNav() {
     return(
-        <nav className="flex md:hidden">
+        <nav className="flex md:hidden fixed bottom-0 inset-x-0 justify-around items-center py-3 bg-background text-foreground border-t border-brand-lavanda/20">
             <Link href="/helpdesk">Help Desk</Link>
             <Link href="/eventi">Eventi</Link>
             <Link href="/">Home</Link>
